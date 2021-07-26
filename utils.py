5f5ac8b1a18f07f63719fb956abfe7ae99fbb29f
trm2yf <orioles810@yahoo.com>
2021-07-26 19:05:37 -0400

    Updated utils.py with doc strings

--- utils.py
+++ utils.py
@@ -1,26 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 from osgeo import gdal
 import os
 import numpy as np
 import requests
 import matplotlib.pyplot as plt
 import pickle
-
-
-# In[ ]:
-
-
-#todo make this default if no datadir passed
-defaultDataDir = '/content/drive/MyDrive/MAXAR/'
-
-
-# In[ ]:
-
+from math import ceil
+import simplekml
+from google.colab import files
 
 #check if we imported module
 def test():
@@ -30,53 +19,208 @@ def test():
   print("Hello World")
 
 
-# In[ ]:
+def download_image(path, datadir = '/content/drive/MyDrive/MAXAR/', time = 'pre', disaster_folder_name = ''):
+    
+    """
+    This function downloads one image from the MAXAR Open Data Program using the https://opendata.digitalglobe.com URL ending in .tif
+    Data gets outputted to the provided datadir within the given disaster_name
+    Function can be used by looping through a list of paths
+    """
+
+    filename = path.split("/")[-1:][0]
+
+    r = requests.get(path, stream = True) 
+
+    with open(datadir + disaster_folder_name + '/{}/{}'.format(time, filename), "wb") as file: 
+        for block in r.iter_content(chunk_size = 1024): 
+            if block: 
+                file.write(block)
+            
+#function to check overlap between images
+def overlap_check(post_path, pre_path):
+    
+    """
+    This function checks whether two downloaded images share any overlapping area
+    If so, it outputs a list containing the pre and post filepaths and the bounding box for the overlapping area in terms of latlongs
+    Function can be used by looping over all combinations of pre and post images (double for loop)
+    """
+    
+    # get post bounds
+    post = gdal.Open(post_path)
+    width = post.RasterXSize
+    height = post.RasterYSize
+    gt = post.GetGeoTransform()
+    post_minx = gt[0]
+    post_miny = gt[3] + width*gt[4] + height*gt[5] 
+    post_maxx = gt[0] + width*gt[1] + height*gt[2]
+    post_maxy = gt[3]
+
+    # get pre bounds
+    pre = gdal.Open(pre_path)
+    width = pre.RasterXSize
+    height = pre.RasterYSize
+    gt = pre.GetGeoTransform()
+    pre_minx = gt[0]
+    pre_miny = gt[3] + width*gt[4] + height*gt[5] 
+    pre_maxx = gt[0] + width*gt[1] + height*gt[2]
+    pre_maxy = gt[3]
+
+    # compare values
+    check_minx = (post_minx <= pre_minx) & (post_maxx >= pre_minx)
+    check_miny = (post_miny <= pre_miny) & (post_maxy >= pre_miny)
+    check_maxx = (post_minx <= pre_maxx) & (post_maxx >= pre_maxx)
+    check_maxy = (post_miny <= pre_maxy) & (post_maxy >= pre_maxy)
+
+    # check if overlap exists
+    overlap = (check_minx|check_maxx) & (check_miny|check_maxy)
+
+    # get image names for output
+    post_path_name = post_path.split("/")[7]
+    pre_path_name = pre_path.split("/")[7]
+
+    # get bounds of overlap, if it exists
+    if overlap:
+        if check_minx:
+            overlap_minx = pre_minx
+        else:
+            overlap_minx = post_minx
+
+        if check_miny:
+            overlap_miny = pre_miny
+        else:
+            overlap_miny = post_miny
+
+        if check_maxx:
+            overlap_maxx = pre_maxx
+        else:
+            overlap_maxx = post_maxx
+
+        if check_maxy:
+            overlap_maxy = pre_maxy
+        else:
+            overlap_maxy = post_maxy
+
+        print("Overlap found between: post/{} and pre/{}!".format(post_path_name, pre_path_name))
+        return [post_path, pre_path, [overlap_minx, overlap_maxy, overlap_maxx, overlap_miny]]
+
+def crop_images_iter(overlap, datadir = '/content/drive/MyDrive/MAXAR/', disaster_folder_name = '', pad = 0.0047, fringe = False):
+    """
+    This functions takes in the list provided by the `overlap_check` function and cuts large image pairs into many smaller image pairs with designated pad value
+    Requires a datadir and disaster_folder_name
+    Function can be used to cut all overlapping areas using a for loop
+    Fringe value dictates whether we want to create additional pictures for overhang area of overlapping region that isn't captured by the final images along the right and bottom. WILL CAUSE DUPLICATED AREA TO BE COVERED, BUT IS THE ONLY WAY TO ENSURE 100% COVERAGE OF OVERLAP IS SEGMENTED INTO SMALLER IMAGES. Typically this won't be an issue because the borders of the overlapping region for at least one of the images will contain the black outskirts of the mask which contain no data. Recommended to leave as False.
+    """
+    
+    post_path = overlap[0]
+    pre_path = overlap[1]
+    bbox = overlap[2]
+
+    x_count = ceil(abs((bbox[0]-bbox[2])/pad))
+    y_count = ceil(abs((bbox[1]-bbox[3])/pad))  
+
+    if not fringe:
+        x_count = x_count - 1
+        y_count = y_count - 1
+
+    post_path_name = post_path.split("/")[7].split(".")[0]
+    pre_path_name = pre_path.split("/")[7].split(".")[0]
+    #added exists = true so we still loop through other wise we get error.
+    os.makedirs(datadir + disaster_folder_name+ '/iter_overlaps/post{}_pre{}'.format(post_path_name, pre_path_name), exist_ok=True)
+
+    for i in range(y_count):
+        for j in range(x_count):
+            # add to leftmost range
+            xmin = bbox[0] + (pad * j)
+            xmax = bbox[0] + (pad * (j+1))
+
+            # subtract from topmost range
+            ymin = bbox[1] - (pad * (i+1))
+            ymax = bbox[1] - (pad * i)
+
+            if fringe:
+                # check if we are in the extra region on the right and adjust accordingly
+                if j+1 == x_count:
+                    xmin = bbox[2] - pad
+                    xmax = bbox[2]
+
+                # check if we are in the extra region on the bottom and adjust accordingly
+                if i+1 == y_count:
+                    ymin = bbox[3] 
+                    ymax = bbox[3] + pad
+
+            bbox_iter = [xmin, ymax, xmax, ymin]
+
+            post = gdal.OpenEx(post_path)
+            post = gdal.Translate(datadir + disaster_folder_name + '/iter_overlaps/post{}_pre{}/{}-{}_post.png'.format(post_path_name, pre_path_name, j+1, i+1),
+                              post,
+                              projWin = bbox_iter,
+                              maskBand = 'none',
+                              format = 'PNG',
+                              width = 480,
+                              height = 480)
+
+            post = None
+
+            pre = gdal.OpenEx(pre_path)
+            pre = gdal.Translate(datadir + disaster_folder_name+ '/iter_overlaps/post{}_pre{}/{}-{}_pre.png'.format(post_path_name, pre_path_name, j+1, i+1),
+                              pre,
+                              projWin = bbox_iter,
+                              maskBand = 'none',
+                              format = 'PNG',
+                              width = 480,
+                              height = 480)
+            pre = None
 
 
-#crop images
-def crop_images_rand(datadir, overlap, pad = 0.0047):
-  post_path = overlap[0]
-  pre_path = overlap[1]
-  bbox = overlap[2]
+            print('finished {}+{}, {}-{}'.format(post_path_name, pre_path_name, j+1, i+1))
 
-  post_path_name = post_path.split("/")[7].split(".")[0]
-  pre_path_name = pre_path.split("/")[7].split(".")[0]
-  #added exists = true so we still loop through other wise we get error.
-  os.makedirs(datadir + 'hurricane_florence/testfolder/post{}_pre{}'.format(post_path_name, pre_path_name), exist_ok=True)
-  # os.makedirs(datadir + 'hurricane_florence/overlaps/post{}_pre{}'.format(post_path_name, pre_path_name), exist_ok=True)
+def crop_images_rand(overlap, datadir = '/content/drive/MyDrive/MAXAR/', disaster_folder_name = '', pad = 0.0047):
+    """
+    This functions takes in the list provided by the `overlap_check` function and cuts a single, random small image pair from a large image pair with designated pad value
+    Requires a datadir and disaster_folder_name
+    Function can be used to cut many random samples of overlapping area
+    Recommended that the `crop_images_iter` version be used for production (while `crop_images_rand` can be useful for testing)
+    """
+    post_path = overlap[0]
+    pre_path = overlap[1]
+    bbox = overlap[2]
 
-  xmin = np.random.uniform(bbox[0], bbox[2]-pad)
-  xmax = xmin+pad
-  ymin = np.random.uniform(bbox[3], bbox[1]-pad)
-  ymax = ymin + pad
+    post_path_name = post_path.split("/")[7].split(".")[0]
+    pre_path_name = pre_path.split("/")[7].split(".")[0]
+    #added exists = true so we still loop through other wise we get error.
+    os.makedirs(datadir + disaster_folder_name + '/rand_overlaps/post{}_pre{}'.format(post_path_name, pre_path_name), exist_ok=True)
 
-  bbox_rand = [xmin, ymax, xmax, ymin]
+    xmin = np.random.uniform(bbox[0], bbox[2]-pad)
+    xmax = xmin+pad
+    ymin = np.random.uniform(bbox[3], bbox[1]-pad)
+    ymax = ymin + pad
 
-  post = gdal.OpenEx(post_path)
-  post = gdal.Translate(datadir + 'hurricane_florence/testfolder/post{}_pre{}/crop_post_{}.png'.format(post_path_name, pre_path_name, post_path_name),
+    bbox_rand = [xmin, ymax, xmax, ymin]
+
+    post = gdal.OpenEx(post_path)
+    post = gdal.Translate(datadir + disaster_folder_name + '/rand_overlaps/post{}_pre{}/crop_post_{}.png'.format(post_path_name, pre_path_name, post_path_name),
                       post,
                       projWin = bbox_rand,
+                      maskBand = 'none',
                       format = 'PNG',
                       width = 480,
                       height = 480)
-  
-  post = None
 
-  pre = gdal.OpenEx(pre_path)
-  pre = gdal.Translate(datadir +'hurricane_florence/testfolder/post{}_pre{}/crop_pre_{}.png'.format(post_path_name, pre_path_name, pre_path_name),
+    post = None
+
+    pre = gdal.OpenEx(pre_path)
+    pre = gdal.Translate(datadir + disaster_folder_name + '/rand_overlaps/post{}_pre{}/crop_pre_{}.png'.format(post_path_name, pre_path_name, pre_path_name),
                       pre,
                       projWin = bbox_rand,
+                      maskBand = 'none',
                       format = 'PNG',
                       width = 480,
                       height = 480)
-  pre = None
-
-  
-  print('finished {}+{}'.format(post_path_name, pre_path_name))
+    pre = None
 
 
-# In[ ]:
-
+    print('finished {}+{}'.format(post_path_name, pre_path_name))
+    
 
 #example data
 def downloadTurkeyEq(datadir):
@@ -93,196 +237,33 @@ def downloadTurkeyEq(datadir):
   print("Downloaded Turkey Earthquake images")
 
 
-# In[ ]:
 
-
-#Currently assumes all folders/subfolderes exist
-def download_image(datadir, path, event = "", time = "pre"):
-  
-  filename = path.split("/")[-1:][0]
-
-  r = requests.get(path, stream = True) 
-  
-  with open(datadir + event + "/{}/{}".format(time, filename), "wb") as file: 
-      for block in r.iter_content(chunk_size = 1024): 
-          if block: 
-              file.write(block)
-
-
-# In[ ]:
-
-
-#function to check overlap between images
-def overlap_check(post_path, pre_path):
-  # get post bounds
-  post = gdal.Open(post_path)
-  width = post.RasterXSize
-  height = post.RasterYSize
-  gt = post.GetGeoTransform()
-  post_minx = gt[0]
-  post_miny = gt[3] + width*gt[4] + height*gt[5] 
-  post_maxx = gt[0] + width*gt[1] + height*gt[2]
-  post_maxy = gt[3]
-
-  # get pre bounds
-  pre = gdal.Open(pre_path)
-  width = pre.RasterXSize
-  height = pre.RasterYSize
-  gt = pre.GetGeoTransform()
-  pre_minx = gt[0]
-  pre_miny = gt[3] + width*gt[4] + height*gt[5] 
-  pre_maxx = gt[0] + width*gt[1] + height*gt[2]
-  pre_maxy = gt[3]
-
-  # compare values
-  check_minx = (post_minx <= pre_minx) & (post_maxx >= pre_minx)
-  check_miny = (post_miny <= pre_miny) & (post_maxy >= pre_miny)
-  check_maxx = (post_minx <= pre_maxx) & (post_maxx >= pre_maxx)
-  check_maxy = (post_miny <= pre_maxy) & (post_maxy >= pre_maxy)
-
-  # check if overlap exists
-  overlap = (check_minx|check_maxx) & (check_miny|check_maxy)
-
-  # get image names for output
-  post_path_name = post_path.split("/")[7]
-  pre_path_name = pre_path.split("/")[7]
-
-  # get bounds of overlap, if it exists
-  if overlap:
-    if check_minx:
-      overlap_minx = pre_minx
-    else:
-      overlap_minx = post_minx
-
-    if check_miny:
-      overlap_miny = pre_miny
-    else:
-      overlap_miny = post_miny
-
-    if check_maxx:
-      overlap_maxx = pre_maxx
-    else:
-      overlap_maxx = post_maxx
-
-    if check_maxy:
-      overlap_maxy = pre_maxy
-    else:
-      overlap_maxy = post_maxy
-       
-    print("Overlap found between: post/{} and pre/{}!".format(post_path_name, pre_path_name))
-    return [post_path, pre_path, [overlap_minx, overlap_maxy, overlap_maxx, overlap_miny]]
-
-  # else:
-    # print("\n no overlap found between: post/{} and pre/{}".format(post_path_name, pre_path_name))
-
-
-# In[ ]:
-
-
-#TODO
-def pre_post_Overlaps():
-  # Calc overlaps for all pre post images
-
-  if calcOverlaps:
-  #perform checks
-    overlaps = []
-    for i in post_images:
-      for j in pre_images:
-        check = utils.overlap_check(i, j)
-        if check is not None:
-          overlaps.append(check)
-      #print("post image "+i+" comparisons done")
-    print("Found ", len(overlaps), "Overlaps")
-
-  #save output to pickle so we dont have to rerun
-    if savetoPickle:
-      with open(datadir + 'overlaps.pkl', 'wb') as f:
-        pickle.dump(overlaps,f)
-      print("Pickled overlaps to", datadir + 'overlaps.pkl')
+def create_kml(overlaps, datadir = '/content/drive/MyDrive/MAXAR/', subfolder = 'kml', filename = '', region_color = '990000ff', download = True):
+    """
+    This function takes as input the list of overlaps and uses it to create a kml file that can be uploaded to Google Maps (or Google Earth)
+    From there you can see on an interactive map which areas you have coverage for in the overlapping regions
+    The end of the function prints directions to follow to work with the now downloaded .kml file
+    """
     
-  else:
-    print("No checks done")
-
-
-# In[ ]:
-
-
-#todo
-#takes root folder, overlap list and event name
-#saves as a 480x480 png
-def crop_images_rand(datadir,overlap, event= "", pad = 0.0047):
-  post_path = overlap[0]
-  pre_path = overlap[1]
-  bbox = overlap[2]
-
-  post_path_name = post_path.split("/")[7].split(".")[0]
-  pre_path_name = pre_path.split("/")[7].split(".")[0]
-  #added exists = true so we still loop through other wise we get error.
-  os.makedirs(datadir + event + '/testfolder/post{}_pre{}'.format(post_path_name, pre_path_name), exist_ok=True)
-  # os.makedirs(datadir + 'hurricane_florence/overlaps/post{}_pre{}'.format(post_path_name, pre_path_name), exist_ok=True)
-
-  xmin = np.random.uniform(bbox[0], bbox[2]-pad)
-  xmax = xmin+pad
-  ymin = np.random.uniform(bbox[3], bbox[1]-pad)
-  ymax = ymin + pad
-
-  bbox_rand = [xmin, ymax, xmax, ymin]
-
-  post = gdal.OpenEx(post_path)
-  post = gdal.Translate(datadir + event + '/testfolder/post{}_pre{}/crop_post_{}.png'.format(post_path_name, pre_path_name, post_path_name),
-                      post,
-                      projWin = bbox_rand,
-                      format = 'PNG',
-                      width = 480,
-                      height = 480)
-  
-  post = None
-
-  pre = gdal.OpenEx(pre_path)
-  pre = gdal.Translate(datadir + event + '/testfolder/post{}_pre{}/crop_pre_{}.png'.format(post_path_name, pre_path_name, pre_path_name),
-                      pre,
-                      projWin = bbox_rand,
-                      format = 'PNG',
-                      width = 480,
-                      height = 480)
-  pre = None
-
-  
-  print('finished {}+{}'.format(post_path_name, pre_path_name))
-
-
-# In[ ]:
-
-
-def plot_image(path):
-  dataset = gdal.OpenEx(path, gdal.GA_ReadOnly) 
-  # Note GetRasterBand() takes band no. starting from 1 not 0
-  band = dataset.GetRasterBand(1)
-  arr = band.ReadAsArray()
-  plt.imshow(arr)
-
-
-# In[ ]:
-
-
-def size_check(path):
-  test = gdal.Open(path)
-  width = test.RasterXSize
-  height = test.RasterYSize
-  gt = test.GetGeoTransform()
-  minx = gt[0]
-  miny = gt[3] + width*gt[4] + height*gt[5] 
-  maxx = gt[0] + width*gt[1] + height*gt[2]
-  maxy = gt[3]
-
-  return print(maxx-minx) 
-  print("/n") 
-  print(maxy-miny)
-
-
-# In[ ]:
-
-
-if __name__ == "__main__":
-   get_ipython().system(u"jupyter nbconvert '/content/drive/MyDrive/MAXAR/utils.ipynb' --to python ")
+    kml = simplekml.Kml()
+    count = 1
+    for i in overlaps:
+        pol = kml.newpolygon(name="Overlap "+str(count),
+                          outerboundaryis=[(i[2][0], i[2][1]), (i[2][2], i[2][1]), (i[2][2], i[2][3]), (i[2][0], i[2][3]), (i[2][0], i[2][1])])
+        pol.style.polystyle.color = region_color  # default is transparent red
+        count += 1
+    kml.save(datadir+subfolder+"/"+filename+".kml")
+    if download:
+        files.download(datadir+subfolder+"/"+filename+".kml")
+    
+    print("""
+    Once downloaded: \n\n
+    -navigate to https://www.google.com/maps/d/ \n
+    -click "+ CREATE A NEW MAP" button \n
+    -click "Import" hyperlink in default first "Untitled layer" \n
+    -upload .kml file from downloads folder \n
+    \n 
+    You can now see the various areas where we have available satellite imagery from before and after the event. Note that these overlapping areas can overlap because we have multiple pre and post image pairs that cover the same overlapping area.
+    
+    """)
 
